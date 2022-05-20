8f2519128d79a4744a53e26a92cf85a483cb6b1a
anusha <anurekha999>
2022-05-20 16:19:46 +0000

    Popularpepos test Completed

---
+++ src/components/RepositoryItem/index.js
@@ -0,0 +1,46 @@
+// Write your code here
+import './index.css'
+
+const RepositoryItem = props => {
+  const {repositoryDetails} = props
+  const {
+    name,
+    imageUrl,
+    starsCount,
+    forksCount,
+    issuesCount,
+  } = repositoryDetails
+
+  return (
+    <li className="repository-item">
+      <img src={imageUrl} className="repository-img" alt={name} />
+      <h1 className="repository-name">{name}</h1>
+      <div className="stars-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/stars-count-img.png"
+          alt="stars"
+          className="star-icon"
+        />
+        <p className="stars-text">{starsCount} stars</p>
+      </div>
+      <div className="stars-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/forks-count-img.png"
+          alt="forks"
+          className="star-icon"
+        />
+        <p className="stars-text">{forksCount} forks</p>
+      </div>
+      <div className="stars-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/issues-count-img.png "
+          alt=" open issues"
+          className="star-icon"
+        />
+        <p className="stars-text">{issuesCount} open issues</p>
+      </div>
+    </li>
+  )
+}
+
+export default RepositoryItem
